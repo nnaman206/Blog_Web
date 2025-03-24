a639136ae2536a1bd020fa2676e13c7484a57888
Naman Goel <nnaman25@gmail.com>
2025-03-24 22:18:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import './App.css'
+import React,{useState,useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import'./App.css'
+import {login,logout} from "./store/authSlice"
+import authService from './AppWrite/auth'
+import {Footer,Header} from './components'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+ const[loading,setLoading]=useState(true)
+ const dispatch=useDispatch()
 
-  return (
-    <>
-     
-    </>
-  )
+ useEffect(()=>
+{
+  authService.getCurrentUser()
+  .then((userData)=>
+  {
+    if(userData)
+    {
+      dispatch(login(userData))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>setLoading(false))
+},[])
+ return !loading?(
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className='w-full block'>
+       <Header/>
+       <main>
+        TODO:<Outlet/>
+       </main>
+       <Footer/>
+    </div>
+  </div>
+ ):null
 }
 
 export default App
--- src/AppWrite/database.js
+++ src/AppWrite/database.js
@@ -1,10 +1,129 @@
 import config from "../config/config";
 import {Client,ID,Databases,Storage,Query} from "appwrite";
 
-export class AuthService{
-   const client=new Client();
-   Databases;
+export class Service{
+    client=new Client();
+   Database;
    storage;
 
    constructor()
+   {
+    this.client
+    .setEndpoint(config.appWriteUrl)
+    .setProject(config.appWriteProjectId);
+    this.Database=new Databases(this.client);
+    this.storage=new Storage(this.client);
+   }
+   async createPost({title,slug,content,featuredImage,status,userID})
+   {
+    try
+    {
+        return await this.Database.createDocument(
+            config.appWriteDatabaseId,
+            config.appWriteCollectionId,
+            slug,
+            {title,
+                content,
+                featuredImage,
+                status,
+                userID,
+            }
+
+        )
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+   }
+   async updatePost(slug,{title,content,featuredImage,status})
+   {
+    try{
+        return await this.Database.updateDocument(
+            config.appWriteDatabaseId,
+            config.appWriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+            }
+        )
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+   }
+   async deletePost(slug)
+   {
+    try{
+        await this.database.deleteDocument(
+            config.appWriteDatabaseId,
+            config.appWriteCollectionId,
+            slug
+        )
+        return true;
+    }catch(error)
+    {
+        console.log(error);
+        return false;
+    }
+   }
+   async getPost(slug)
+   {
+    try{
+        return await this.database.getDocument(
+            config.appWriteDatabaseId,
+            config.appWriteCollectionId,
+            slug
+        )
+    }catch(error)
+    {
+        console.log(error)
+        return false;
+    }
+   }
+   async uploadFile(file)
+   {
+    try{
+        return await this.storage.createFile(
+            config.appWriteBuketId,
+            ID.unique(),
+            file
+        )
+    }catch(error)
+    {
+        console.log(error)
+        return false;
+    }
+   }
+   async deleteFile(fileId)
+   {
+    try{
+        await this.storage.deleteFile(
+            config.appWriteBuketId,
+            fileId
+        )
+    }catch(error)
+    {
+        console.log(error)
+        return false;
+    }
+   }
+   getFilePreview(fileId)
+   {
+    return this.storage.getFilePreview(
+        config.appWriteBuketId,
+        fileId
+    )
+   }
+
+
+
+
 }
+
+const service=new Service ()
+export default service;
